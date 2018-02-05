503dfd5526290f2f924796d1721eede079c6a60a
David Foster <david@dafoster.net>
2018-02-04 20:55:30 -0800

    Remove unused poll app. Remove unused submodules of chat app.

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^chat/', include('chat.urls')),
-    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
